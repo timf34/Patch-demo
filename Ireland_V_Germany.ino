e5561950c8f060d442f0cae2d1228c06e9c8674c
timf34 <66926418+timf34@users.noreply.github.com>
2020-08-06 14:44:10 +0100

    Update Ireland_V_Germany.ino
    Do you wanna just try that out and see what it looks like.

--- Ireland_V_Germany.ino
+++ Ireland_V_Germany.ino
@@ -127,12 +127,12 @@ void loop() {
  // stepper1.setMaxSpeed(2000);
  // stepper2.setMaxSpeed(2000);
   
-
+ //const int size= 10
 float wCoordinates [35] = {5.5, 5.7, 6, 5.9, 6.1, 6.5, 7, 7.2, 7.3, 6.5, 6.2, 6, 8, 7.7, 7.6, 7.4, 6.8, 5.5, 5.2, 5.3, 5.4, 5.2, 5.3, 5.4, 5.8, 6, 6.1, 6.2, 6.3, 6.4, 4.8, 4.7, 4.7, 5, 5};
 float lCoordinates [35] = {4.5, 4, 3.7, 3, 2.5, 2.6, 2.6, 2.6, 2.3, 1.9, 1.7, 1.4, 1.7, 1.8, 1.8, 2.7, 3.7, 4.2, 4.7, 5.2, 5.8, 6.3, 7.5, 8, 9, 9, 6.5, 6, 4.5, 3.5, 2.4, 1.8, 1.7, 0.3, 0,};
 
- stepper1.setMaxSpeed(7000);
-  stepper2.setMaxSpeed(7000);
+//float wCoordinates [10] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0 ,7.0 ,8.0 ,9.0 , 10.0};
+//float lCoordinates [10] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0 ,7.0 ,8.0 ,9.0 , 10.0};
   
 long positions[2];
 
@@ -142,7 +142,7 @@ long positions[2];
  steppers.runSpeedToPosition(); // Blocks until all are in position
  
 
-for (int i = 0; i <= 35; i++) {
+for (int i = 0; i <= 10; i++) {
  
   float widthW = wCoordinates[i];
 float lengthL = lCoordinates[i];
